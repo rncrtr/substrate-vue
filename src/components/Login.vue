1462611b56800978b7d44e79e1ecaec0b5df2b33
Ryvan Crazinov <ryvan@Crazys-MacBook.local>
2017-10-02 09:36:03 -0600

    init

---
+++ src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+	<div id="login">
+		<h2>Subtitle</h2>
+		<h2 class="subtitle">Bulma Subtitle</h2>
+		<button>Normal</button>
+		<button class="button is-primary">Bulma Button</button>
+	</div>
+</template>
+<script>
+	export default {
+		name: 'login'
+	}
+</script>
+<style>
+	
+</style>
---
+++ src/main.js
@@ -0,0 +1,35 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import VueRouter from 'vue-router'
+
+// Import Components
+import Hello from './components/Hello'
+import About from './components/About'
+import Login from './components/Login'
+
+require('./assets/sass/main.scss');
+
+Vue.config.productionTip = false
+
+Vue.use(VueRouter)
+
+const routes = [
+	{ path: '/', component: Hello },
+	{ path: '/about', component: About },
+	{ path: '/login', component: Login },
+]
+
+const router = new VueRouter({
+  routes, // short for routes: routes
+  mode: 'history'
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})

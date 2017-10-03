c8e5a8581d001d7dfd081cf711ca24cefeabe16c
Ryan Carter <ryan.p.carter@charter.com>
2017-10-03 12:43:04 -0600

    init

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,9 +1,6 @@
 <template>
 	<div id="login">
-		<h2>Subtitle</h2>
-		<h2 class="subtitle">Bulma Subtitle</h2>
-		<button>Normal</button>
-		<button class="button is-primary">Bulma Button</button>
+		
 	</div>
 </template>
 <script>
